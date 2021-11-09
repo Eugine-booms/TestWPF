48f6d449af671bddcc0178b5958fe5f34487d1b3
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-09 12:18:20 +0300

    Базовая реализация команды

---
+++ Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace TestWPFApp.Infrastructure.Commands.Base
+{
+    internal abstract class Command : ICommand
+    {
+        /// <summary>
+        /// Событие генерируется когда меняется состояние CanExecute
+        /// </summary>
+        public event EventHandler CanExecuteChanged
+        {
+            //передача управление системе 
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        /// <summary>
+        /// Говорит может быть выполнена команда в зависимости от параметра
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns>true - команда может быть выполнена, false - не может </returns>
+        public abstract bool CanExecute(object parameter);
+
+        /// <summary>
+        /// Выполнение команды
+        /// </summary>
+        /// <param name="parameter"></param>
+        public abstract void Execute(object parameter);
+        
+    }
+}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -8,18 +8,38 @@ namespace TestWPFApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
+        
+        #region Title : string  - Заголовок окна
 
-        private string _Title = "New Window";
+
+        private string _title = "New Window";
 
         /// <summary> Заголовок окна </summary>
         public string Title
         {
-            get => _Title;
-            set => Set(ref _Title, value);
+            get => _title;
+            set => Set(ref _title, value);
             //то что мы должны были писать но нам помог базовый класс VM
             //if (Equals(_Title, value)) return;
             //_Title = value;
             //OnPropertyChanged();
         }
+        #endregion
+
+        #region status : string  - Статус программы
+        ///<summary> Статус программы
+        private string _status= "Готов";
+        ///<summary> Статус программы
+        public string Status
+        {
+            get => _status;
+            set => Set(ref _status, value);
+        }
+        #endregion
+
+
+
+
+
     }
 }
