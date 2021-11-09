8b40713f47493a95145b9dace44e98bc74c126f0
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-09 16:47:12 +0300

    Добавляем в VM данные для графиков

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Input;
 using TestWPFApp.Infrastructure.Commands;
+using TestWPFApp.Model;
 using TestWPFApp.ViewModels.Base;
 
 
@@ -11,7 +13,18 @@ namespace TestWPFApp.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        
+
+        #region testDataPoint : IEnumerable  - Тестовый набор данных для визуализации графиков
+        ///<summary> Точки графика
+        private IEnumerable<DataPoint> _testDataPoint;
+        ///<summary> Точки графика
+        public IEnumerable<DataPoint> TestDataPoint
+        {
+            get => _testDataPoint;
+            set => Set(ref _testDataPoint, value);
+        }
+        #endregion
+
         #region Title : string  - Заголовок окна
 
 
@@ -31,9 +44,9 @@ namespace TestWPFApp.ViewModels
 
         #region status : string  - Статус программы
         ///<summary> Статус программы
-        private string _status= "Готов";
+        private string _status = "Готов";
 
-        
+
 
         ///<summary> Статус программы
         public string Status
@@ -44,27 +57,32 @@ namespace TestWPFApp.ViewModels
         #endregion
 
         #region Команды
+
+
         #region CloseAppCommand
 
-        
+
         public ICommand CloseAppCommand { get; }
         /// <summary>
         /// Может ли выполниться команда
         /// </summary>
         /// <param name="p"></param>
         /// <returns>Может ли выполниться команда</returns>
-        private bool CanCloseAppCommandExicute(object p)  => true;
+        private bool CanCloseAppCommandExicute(object p) => true;
         /// <summary>
         /// Выполняется в момент выполнения команды
         /// </summary>
         /// <param name="p"></param>
-        private void OnCloseAppCommandExicuted(object p) 
+        private void OnCloseAppCommandExicuted(object p)
         {
             Application.Current.Shutdown();
         }
         #endregion
 
         #endregion
+
+        #region Конструктор 
+
         public MainWindowViewModel()
         {
             #region Command
@@ -72,8 +90,26 @@ namespace TestWPFApp.ViewModels
             CloseAppCommand = new LambdaCommand(OnCloseAppCommandExicuted, CanCloseAppCommandExicute);
 
             #endregion
-        }
 
+            TestDataPoint = GenerateTestDataPoint(); ;
+        }
+        #endregion
+
+        #region Вспомогательные методы
+
+        
+        private static List<DataPoint> GenerateTestDataPoint()
+        {
+            var data_points = new List<DataPoint>((int)(360 / 0.1));
+            for (var x = 0d; x <= 360; x += 0.1)
+            {
+                const double to_rad = Math.PI / 180;
+                var y = Math.Sin(x * to_rad);
+                data_points.Add(new DataPoint { XValue = x, YValue = y });
+            }
+            return data_points;
+        }
+        #endregion
 
 
     }
