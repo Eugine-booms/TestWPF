6b9c4aa86e2f0f05be94ffd60f513e1d97fb4573
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-09 13:03:18 +0300

    Реализация команды отдельно от VM CloseAppCommand

---
+++ Infrastructure/Commands/CloseAppCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using TestWPFApp.Infrastructure.Commands.Base;
+
+namespace TestWPFApp.Infrastructure.Commands
+{
+    internal class CloseAppCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+        
+
+        public override void Execute(object parameter)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
