ccc743a58c90a40081352cbb21ec6e4617314838
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-10 12:15:13 +0300

    Тестовая модель деканат

---
+++ TestWPF/Model/Decant/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace TestWPFApp.Model.Decant
+{
+    internal    class Student
+
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronumic { get; set; }
+        public DateTime Birthday { get; set; }
+        public double Rating { get; set; }
+    }
+}
