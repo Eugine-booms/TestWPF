238dc88bd7cab0839256f24ec1332ae7876aa72f
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-11 12:29:17 +0300

    Добавлен фильтр групп в V и VM

--- TestWPF/View/Windows/MainWindow.xaml.cs
+++ TestWPF/View/Windows/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TestWPFApp.Model.Decant;
 
 namespace TestWPFApp
 {
@@ -25,6 +26,25 @@ namespace TestWPFApp
             InitializeComponent();
         }
 
-        
+        private void GroupCollectionFilter(object sender, FilterEventArgs e)
+        {
+            //Если e.Item не группа - ничего не делаем
+            if (!(e.Item is Group group )) return;
+            if (group.Name is null) return;
+            //получаем строку из TextBox 
+            
+            var filterText = GroupNameFilterText.Text;
+            if (filterText.Length == 0) return;
+            if (group.Name.Contains(filterText, StringComparison.OrdinalIgnoreCase)) return;
+            //if (group.De)
+            e.Accepted = false;
+        }
+
+        private void GroupNameFilterText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            var collection = textBox.FindResource("GroupsCollection") as CollectionViewSource;
+            collection.View.Refresh();
+        }
     }
 }
