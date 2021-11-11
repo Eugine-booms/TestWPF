ed3accf6e90ec392ff5198ac3c3764c6cce9f69f
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-11 19:34:33 +0300

    VM Directory and file

---
+++ TestWPF/ViewModels/DirectoryViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TestWPFApp.ViewModels.Base;
+
+
+namespace TestWPFApp.ViewModels
+{
+    internal class DirectoryViewModel : ViewModel
+    {
+        private readonly DirectoryInfo directoryInfo;
+
+        public IEnumerable<DirectoryViewModel> SubDirectories
+        {
+            get => directoryInfo
+                .EnumerateDirectories()
+                .Select(dir_info => new DirectoryViewModel(dir_info.FullName));
+        }
+        public DirectoryViewModel(string path)
+        {
+            directoryInfo = new DirectoryInfo(path);
+        }
+        public IEnumerable<FileViewModel> Files => directoryInfo
+            .EnumerateFiles()
+            .Select(file => new FileViewModel(file.FullName));
+        public string Name => directoryInfo.Name;
+        public string Path => directoryInfo.FullName;
+        public DateTime CreationTime => directoryInfo.CreationTime;
+        public IEnumerable<object> DirerectoryItems
+        {
+            get => SubDirectories.Cast<object>().Concat(Files.Cast<object>());
+
+        }
+    }
+    class FileViewModel: ViewModel
+    {
+        private readonly FileInfo fileInfo;
+
+        public string Name => fileInfo.Name;
+        public string Path => fileInfo.FullName;
+        public DateTime CreationTime => fileInfo.CreationTime;
+        public FileViewModel(string path)
+        {
+            fileInfo = new FileInfo(path);
+        }
+    }
+    
+}
