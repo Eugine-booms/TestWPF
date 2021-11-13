cbbab54b6b10d9cef39fc091a1bd3751eb4a71d8
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-13 18:03:31 +0300

    Создание конвертера (между Point и строкой)

---
+++ TestWPF/Infrastructure/Converters/LocationPointToStr.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+    internal class LocationPointToStr : IValueConverter
+    {
+        public object Convert(object value, Type t, object p, CultureInfo c)
+        {
+            if (!(value is Point point)) return null;
+            return $"Lat:{point.X};Lon{point.Y}";
+        }
+
+        public object ConvertBack(object value, Type t, object p, CultureInfo c)
+        {
+            if (!(value is string str)) return null;
+
+            var component = str.Split(';');
+            var lat_str = component[0].Split(';')[1];
+            var lot_str= component[1].Split(';')[1];
+
+            var lat= double.Parse(lat_str, CultureInfo.InvariantCulture );
+            var lot = double.Parse(lot_str, CultureInfo.InvariantCulture);
+
+            return new Point(lat, lot);
+        }
+    }
+}
