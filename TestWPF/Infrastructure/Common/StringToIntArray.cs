c6e6f5710e1b25b8ef4a6628d177bb47e7a09241
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-15 15:03:33 +0300

    Пример использования расширения разметки как DataContext

---
+++ TestWPF/Infrastructure/Common/StringToIntArray.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Markup;
+
+namespace TestWPFApp.Infrastructure.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [MarkupExtensionReturnType(typeof(int[]))]
+    internal class StringToIntArray : MarkupExtension
+    {
+        [ConstructorArgument("Str")]
+        public string Str { get; set; }
+        [ConstructorArgument("Separator")]
+        public Char Separator { get; set; } = ';';
+
+        public StringToIntArray() { }
+        public StringToIntArray(string str)
+        {
+            Str = str ?? throw new ArgumentNullException(nameof(str));
+        }
+
+        public StringToIntArray(string str, char sep) : this(str)
+        {
+            Separator = sep;
+        }
+
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            var result = Str
+                    .Split(new[] { Separator }, StringSplitOptions.RemoveEmptyEntries)
+                    .DefaultIfEmpty()
+                    .Select(int.Parse)
+                    .ToArray();
+            return result;
+        }
+    }
+}
