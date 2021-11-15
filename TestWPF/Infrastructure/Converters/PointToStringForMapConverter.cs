c4b832a7a0e00c227aaf5ce3efd81ebe5dd864df
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-15 17:08:15 +0300

    Добавлен конвертер для карты Point To Location

---
+++ TestWPF/Infrastructure/Converters/PointToStringForMapConverter.cs
@@ -0,0 +1,28 @@
+﻿using MapControl;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+    [ValueConversion(typeof(Point), typeof(Location))]
+    [MarkupExtensionReturnType(typeof(PointToStringForMapConverter))]
+    internal class PointToStringForMapConverter : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Point point)) return null;
+            return new Location(point.X, point.Y);
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Location location)) return null;
+            return new Point(location.Latitude, location.Longitude);
+        }
+    }
+}
