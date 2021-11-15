11ba89d8b22e90fdea4bfb12366e53dec0a4ffe9
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-15 13:59:51 +0300

    Создание базового Мультиконвертера

---
+++ TestWPF/Infrastructure/Converters/Base/MultiConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+
+    internal  abstract class MultiConverter : IMultiValueConverter
+    {
+        public abstract object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+       
+
+        public virtual object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживаться");
+        }
+    }
+}
