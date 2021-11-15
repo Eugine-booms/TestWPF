226e16d478c07c2d1ebb2763da3ee0af14a9c22c
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-15 14:00:24 +0300

    Создание ToArray MultiConverter

---
+++ TestWPF/Infrastructure/Converters/ToArrayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+    [ValueConversion(typeof(object[]), typeof(double))]
+    internal class ToArrayConverter : MultiConverter
+    {
+        public override object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.ToArray();
+        }
+
+        //public override object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        //{
+        //   // return value as object[];
+        //}
+    }
+}
