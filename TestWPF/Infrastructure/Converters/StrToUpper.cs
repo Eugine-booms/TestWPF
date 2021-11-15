5884cd055470705eaf82e5b0b8283c76f2984e58
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-15 15:11:25 +0300

    Добавлен конвертер строки в верхний регистр

---
+++ TestWPF/Infrastructure/Converters/StrToUpper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    [MarkupExtensionReturnType(typeof(StrToUpper))]
+    internal class StrToUpper : Converter
+    {
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            var str = value as string;
+            return str.ToUpper();
+        }
+    }
+}
