7de722dcea887354925073f1c83cd85f0683fd00
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-16 18:32:55 +0300

    Создание конвертера с праметрами. Обязательно наследовать от Freezable иначе не получит контекст данных очень странная штука

---
+++ TestWPF/Infrastructure/Converters/ParametricMultiplicityValueConverter.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TestWPFApp.Infrastructure.Converters
+{
+    [ValueConversion(typeof(double), typeof(double))]
+    internal class ParametricMultiplicityValueConverter : Freezable, IValueConverter
+    {
+
+
+        public double Value
+        {
+            get { return (double)GetValue(CoefficientProperty); }
+            set { SetValue(CoefficientProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Coefficient.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CoefficientProperty =
+            DependencyProperty.Register(nameof(Value), typeof(double), typeof(ParametricMultiplicityValueConverter), new PropertyMetadata(1.0, OnValuePropertyChanged, OnCoerceValue));
+
+        private static object OnCoerceValue(DependencyObject d, object baseValue)
+        {
+            return baseValue;
+        }
+
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = System.Convert.ToDouble(value, culture);
+            return val * Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var backval = System.Convert.ToDouble(value, culture);
+            return backval / Value;
+        }
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new ParametricMultiplicityValueConverter() { Value = this.Value };
+        }
+    }
+}
