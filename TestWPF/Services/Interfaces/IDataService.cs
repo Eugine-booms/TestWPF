044e013620cddb6e7b25a6ab44b5948362ca1fe3
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-16 12:37:31 +0300

    Выделение интерфейса IDataService из DataService

---
+++ TestWPF/Services/Interfaces/IDataService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestWPFApp.Model;
+
+namespace TestWPFApp.Services.Interfaces
+{
+   internal interface IDataService
+    {
+        public IEnumerable<CountryInfo> GetData();
+    }
+}
