00c3aa0ec787c8dec4a80ec42e611714ab1811ad
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-17 19:05:34 +0300

    Блокировка и взаимная синхронизация потоков

--- Tests/TestWPFConsole/Program.cs
+++ Tests/TestWPFConsole/Program.cs
@@ -16,21 +16,71 @@ namespace TestWPFConsole
         private static bool _threadUp=true;
         static void Main(string[] args)
         {
-            
-            Thread.CurrentThread.Name = "Main Thread";
-            var clock_thread = new Thread(ThreadMetod);
-            clock_thread.Name = "Second Thread";
-            clock_thread.IsBackground = true;
-            var priority = clock_thread.Priority;
-            clock_thread.Start();
 
-            if(!(clock_thread.Join(100))) //синхронизирует поток
+
+            //Mutex mutex = new Mutex(); //если есть несколько приложений то тот кто его создал не первым будет уведомлен о том что он создал не первый
+            //                            /// через него можно производить синхронизацию
+            //Semaphore semaphore = new Semaphore(0,10); //позволяет блокировать критическую секцию через ядро операционной системы
+            //                                           // ограничивает количество входов внутри себя
+            //semaphore.WaitOne();                        // для ограничения нагрузки на сервер НАПРИМЕР
+            ////действия в критической секции
+            //semaphore.Release();
+
+            //как стартовый пистолет для бегунов
+            ManualResetEvent manualResetEvent = new ManualResetEvent(false); //не сбрасывает все делается в ручную set reset
+            AutoResetEvent autoResetEvent = new AutoResetEvent(false); //сбрасывает  thread_guidance.WaitOne(); при прохождении через него в false
+
+            EventWaitHandle thread_guidance = autoResetEvent;
+
+            var test_threads = new Thread[10];
+
+            for (int i = 0; i < test_threads.Length; i++)
             {
-                //   clock_thread.Abort(); //прерывает в любой точке процесса выполнения не поддерживается в настоящее время
-                
-                clock_thread.Interrupt(); //
+                var local_i = i;
+                test_threads[i] = new Thread(() => 
+                {
+                    Console.WriteLine("Поток id:{0}-стартовал", Thread.CurrentThread.ManagedThreadId);
+                    thread_guidance.WaitOne();   //Ждут отмашки thread_guidance.Set();
+                    Console.WriteLine("Value:{0}",local_i);
+                    Console.WriteLine("Поток Id:{0}- завершился", Thread.CurrentThread.ManagedThreadId);
+
+                });
+                test_threads[i].Start();
             }
-            
+
+            Console.WriteLine("Готов к запуску потоков");
+            Console.ReadLine();
+
+            //стартует все потоки
+            thread_guidance.Set();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Thread.CurrentThread.Name = "Main Thread";
+            //var clock_thread = new Thread(ThreadMetod);
+            //clock_thread.Name = "Second Thread";
+            //clock_thread.IsBackground = true;
+            //var priority = clock_thread.Priority;
+            //clock_thread.Start();
+
+            //if(!(clock_thread.Join(100))) //синхронизирует поток
+            //{
+            //    //   clock_thread.Abort(); //прерывает в любой точке процесса выполнения не поддерживается в настоящее время
+
+            //    clock_thread.Interrupt(); //
+            //}
+
 
             //var count = 5;
             //var msg = "Пипец";
@@ -65,7 +115,7 @@ namespace TestWPFConsole
             //            {
             //                list.Add(Thread.CurrentThread.ManagedThreadId);
             //            }
-                       
+
             //        }
 
             //    });
@@ -96,7 +146,7 @@ namespace TestWPFConsole
 
 
 
-            
+
             //Console.WriteLine(string.Join(",", list));
 
             Console.ReadLine();
