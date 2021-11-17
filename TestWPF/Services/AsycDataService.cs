fe9c94db836a845aeb4329b1d0e68e8487bbfcaa
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-17 11:06:20 +0300

    Заготовка асинхронного сервиса данных

---
+++ TestWPF/Services/AsycDataService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TestWPFApp.Services.Interfaces;
+
+namespace TestWPFApp.Services
+{
+    internal class AsycDataService : IAsycDataService
+    {
+    }
+}
