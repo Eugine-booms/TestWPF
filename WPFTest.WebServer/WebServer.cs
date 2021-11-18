831a2ab84a831c2787bcc23d19d4df0ed7151136
Евгений Нестеров <e.y.nesterov@gmail.com>
2021-11-18 09:18:56 +0300

    Заготовка асинхронного сервера

--- WPFTest.WebServer/WebServer.cs
+++ WPFTest.WebServer/WebServer.cs
@@ -1,11 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace WPFTest.WebServer
 {
-   public class WebServer
+    public class WebServer
     {
+        //сложный метод делать много самому напрямую с драйвера  сетевой карты
+        //private TcpListener tcpListener = new TcpListener(IPAddress.Any, 8080);
 
+        //легкий способ (могут быть проблемы с разрешением файвола и доступа к драйверу карты
+        private HttpListener listener;
+        private readonly int port;
+        private bool enabled;
+        private readonly object syncRoot = new object();
+
+        public int Port => port;
+
+        public bool Enabled
+        {
+            get => enabled;
+            set { if (enabled) Start(); else Stop(); }
+        }
+
+        public WebServer(int port)
+        {
+            this.port = port;
+        }
+        public void Start()
+        {
+            if (enabled) return;
+            lock (syncRoot)
+            {
+                if (enabled) return;
+                listener = new HttpListener();
+                listener.Prefixes.Add($"http://*:{Port}/"); //если не разрешено получим исключение
+                listener.Prefixes.Add($"http://+:{Port}/"); //если не разрешено получим исключение
+                Enabled = true;
+            }
+            Listen();
+        }
+
+        private void Listen()
+        {
+            listener.Start();
+            Console.WriteLine("Ожидаем подключения");
+            var listener_content = listener.GetContext();
+            var listener_request = listener_content.Request;
+        }
+
+        public void Stop()
+        {
+            if (!enabled) return;
+            lock (syncRoot)
+            {
+                if (!enabled) return;
+                listener = null;
+                Enabled = false;
+            }
+        }
     }
 }
